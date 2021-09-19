7a43e46601d9655ccddec9a8794a3d6ed8c892ac
Timo Ulich <timo@ulich.net>
2021-09-19 14:24:28 +0200

    support notifying via slack webhook and comparing a specific element against the unwanted text

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # website-change-detector
 
-Checks when a website changes and notifies you via OpsGenie
+Checks when a website changes and notifies you via OpsGenie or Slack
 
 ## Running locally 
 
     /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9223 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')
 
-    export BASEURL="https://the-website-to-check.com"
+    export URL="https://the-website-to-check.com"
+    
+    // if you want to get notified via opsgenie
     export OPSGENIE_API_KEY="xxxxx"
-    export UNWANTED_TEXT="When this text is not found on the page, it must have changed"
+
+    // if you want to get notified via slack
+    export SLACK_WEBHOOK_URL="https://hooks.slack.com/services/xxxxx"
+
+    export CSS_SELECTOR=".some-css-selector"
+    export UNWANTED_TEXT="If the element targeted via CSS_SELECTOR does not have this text, trigger the notification"
     node index.js
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { openBrowser, goto, text, deleteCookies } = require("taiko");
-const axios = require('axios')
+const { openBrowser, goto, text, $, deleteCookies } = require("taiko");
+const axios = require('axios');
 
-;(async () => {
+; (async () => {
   try {
     await openBrowser({
       host: "localhost",
@@ -10,15 +10,18 @@ const axios = require('axios')
     });
 
     await deleteCookies()
-    await goto(process.env.BASEURL);
+    await goto(process.env.URL);
 
-    if (await text(process.env.UNWANTED_TEXT).exists()) {
+    const textFound = (await $(process.env.CSS_SELECTOR).text()).trim()
+    console.log('Text found:', textFound)
+
+    if (textFound === process.env.UNWANTED_TEXT) {
       console.log('Unwanted text found. Site did not change. Exiting...')
       process.exit(0)
     }
 
-    console.log('notifying')
-    await notify()
+    console.log(`notifying`)
+    await notify(textFound)
     process.exit(0)
   } catch (e) {
     console.error(e)
@@ -26,16 +29,32 @@ const axios = require('axios')
   }
 })();
 
-async function notify() {
+async function notify(textFound) {
   try {
-    await axios.post('https://api.eu.opsgenie.com/v2/alerts', {
-      message: 'website changed!'
-    }, {
-      headers: {
-        Authorization: `GenieKey ${process.env.OPSGENIE_API_KEY}`
-      }
-    })
+    if (process.env.OPSGENIE_API_KEY) {
+      await notifyViaOpsGenie(textFound)
+    } else if (process.env.SLACK_WEBHOOK_URL) {
+      await notifyViaSlack(textFound)
+    } else {
+      throw new Error('Neither OPSGENIE_API_KEY nor SLACK_WEBHOOK_URL provided. Exiting')
+    }
   } catch (e) {
-    throw new Error('Error triggering alert: ' + JSON.stringify(e.response?.data))
+    throw new Error('Error triggering alert: ' + JSON.stringify(e.response?.data ?? e.message))
   }
+}
+
+async function notifyViaOpsGenie(textFound) {
+  await axios.post('https://api.eu.opsgenie.com/v2/alerts', {
+    message: `website changed! Text found was: ${textFound}`
+  }, {
+    headers: {
+      Authorization: `GenieKey ${process.env.OPSGENIE_API_KEY}`
+    }
+  })
+}
+
+async function notifyViaSlack(textFound) {
+  await axios.post(process.env.SLACK_WEBHOOK_URL, {
+    text: `Change detected on ${process.env.URL}! Text found was: ${textFound}`
+  })
 }
